4e0e256316eccc1fa9a4927e59bf1220d0dc3b6a
Calmarius <calmarius@calmarius.net>
2015-05-25 12:10:03 +0200

    Readd configuration.php and shadow.php

---
+++ shadow.php
@@ -0,0 +1,6 @@
+<?php
+	define('__MYSQL_USER_NAME','calmarius');
+	define('__MYSQL_PASSWORD','-+-787pspinc');
+	define('__MYSQL_DB_NAME','calmarius');
+	define('__ADMIN_MAIL','calmarius@calmarius.net');
+?>
