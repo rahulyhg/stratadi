d1fa1a6bd0e100071a02f9db93b5035c8f2f1798
Calmarius <calmarius@calmarius.net>
2015-05-25 11:59:27 +0200

    Untrack configuration and shadow.php

--- shadow.php
+++
@@ -1,6 +0,0 @@
-<?php
-	define('__MYSQL_USER_NAME','calmarius');
-	define('__MYSQL_PASSWORD','-+-787pspinc');
-	define('__MYSQL_DB_NAME','calmarius');
-	define('__ADMIN_MAIL','calmarius@calmarius.net');
-?>
