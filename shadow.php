178e4863afe00cdf1775ee29bb3dd963c1b3710a
Calmarius <calmarius@calmarius.net>
2015-06-01 16:15:18 +0200

    Fixed PHP errors.

--- shadow.php
+++ shadow.php
@@ -1,6 +1,6 @@
 <?php
 	define('__MYSQL_USER_NAME','stratadi');
-	define('__MYSQL_PASSWORD','YHPPcNAbvacZYpxR');
+	define('__MYSQL_PASSWORD','stratata');
 	define('__MYSQL_DB_NAME','stratadi');
 	define('__ADMIN_MAIL','calmarius@calmarius.net');
 ?>
