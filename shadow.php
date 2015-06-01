d0c285d11f360961ea5ddd81b1371caed8fb7f2b
Calmarius <calmarius@calmarius.net>
2015-06-01 14:31:57 +0200

    Updated configuration and shadow.

--- shadow.php
+++ shadow.php
@@ -1,6 +1,6 @@
 <?php
-	define('__MYSQL_USER_NAME','calmarius');
-	define('__MYSQL_PASSWORD','-+-787pspinc');
-	define('__MYSQL_DB_NAME','calmarius');
+	define('__MYSQL_USER_NAME','stratadi');
+	define('__MYSQL_PASSWORD','YHPPcNAbvacZYpxR');
+	define('__MYSQL_DB_NAME','stratadi');
 	define('__ADMIN_MAIL','calmarius@calmarius.net');
 ?>
